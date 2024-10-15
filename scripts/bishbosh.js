1551ec8c169d08b74dc190324f739265f1f4db6e
Luna <hanken95@gmail.com>
2024-10-15 18:01:46 +0200

    Finished advanced bishbosh and the site frontend

--- scripts/bishbosh.js
+++ scripts/bishbosh.js
@@ -1,5 +1,9 @@
-document.getElementById("bishBoshButton").onclick = GetStandardBishBoshList;
+document.getElementById("standardBishBoshButton").onclick = GetStandardBishBoshList;
+document.getElementById("advancedBishBoshButton").onclick = AdvancedBishBosh;
 
+let errorMessageText = document.getElementById("errorMessage")
+
+let list = document.getElementById("bishBoshList")
 
 function GetStandardBishBoshList() 
 {
@@ -25,4 +29,77 @@ function GetStandardBishBoshList()
         }
     }
     console.log(bishBoshList)
+}
+
+function AdvancedBishBosh() 
+{
+    let bishValue = Number(document.getElementById("bishNumber").value)
+    let boshValue = Number(document.getElementById("boshNumber").value)
+    let loopMax = Number(document.getElementById("loopMax").value)
+
+
+    if (bishValue == ''  || bishValue == null || boshValue == ''  || boshValue == null || loopMax == ''  || loopMax == null) 
+    {
+        ErrorMessage("Values not entered correctly");
+    }
+    else if (bishValue < 1)
+    {
+        ErrorMessage("Bish number cannot be below 1")
+    }
+    else if (bishValue >= boshValue) 
+    {
+        ErrorMessage("Bish number has to be smaller than Bosh number")
+    }
+    else if (loopMax < boshValue) 
+    {
+        ErrorMessage("Loop max cannot be smaller than Bosh number")
+    }
+    else 
+    {
+        ClearErrorMessage()
+        ClearList(false);
+
+        for (let i = 1; i <= loopMax; i++) 
+        {
+            let listItem = document.createElement('li')
+            if (i % bishValue == 0 && i % boshValue == 0) 
+            {
+                listItem.innerText = 'Bish-Bosh'
+            } 
+            else if (i % bishValue == 0) 
+            {
+                listItem.innerText = 'Bish'
+            }
+            else if (i % boshValue == 0) 
+            {
+                listItem.innerText = 'Bosh'
+            }
+            else
+            {
+                listItem.innerText = i.toString()
+            }
+            list.appendChild(listItem)
+        }
+    }
+}
+
+function ErrorMessage(message)
+{
+    errorMessageText.textContent = message
+}
+
+function ClearErrorMessage()
+{
+    errorMessageText.textContent = ""
+}
+
+function ClearList(addNothingHere)
+{
+    list.innerText = ''
+    if (addNothingHere)
+    {
+        let listItem = document.createElement('li')
+        listItem.innerText = 'Nothing here'
+        list.appendChild(listItem)
+    }
 }
